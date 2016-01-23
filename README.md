2ea6f3afec27e146d022c135f3c8fabf975bd805
Shahar Talmi <shahart@wix.com>
2016-01-23 21:41:46 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+### Why do I need this?
+
+Easily integrate protractor with eyes when using jasmine2 as testing framework. All you have to do is use `eyes.it` in your tests instead of `it` and it will run the test with screenshots sent to eyes after each `browser.get()` and at the end of the test.
+
+### Installing
+
+```sh
+npm install --save-dev eyes.it
+```
+
+### Usage
+
+Add environment variable with your eyes api key
+```sh
+export EYES_API_KEY=6QGH9IA5nkK1wRt60I1EWybFMWTJ2R1kcwu07y41lYh0LNWu3r
+```
+
+In your protractor tests:
+```js
+let eyes = require('eyes.it');
+
+eyes.it('should run tests with eyes', () => {
+    browser.get('/');
+    $('input').sendKeys('123');
+    $('button').click();
+    expect($('span').text()).toBe('123');
+});
+```
+
+In case you require more screenshots in addition to the default ones that happen after browser.get() and at the end of the test, you can always call `eyes.checkWindow(testName);` in your test on your own.
+
+You can also use `eyes.fit` in case you need to use focused tests.
---
+++ index.js
@@ -0,0 +1,50 @@
+/* global fit */
+'use strict';
+
+var Eyes = require('eyes.protractor').Eyes;
+var eyes = new Eyes();
+var path = require('path');
+
+var appName = require(path.join(process.cwd(), 'package.json')).name;
+var eyesOpen = false;
+var hooked = browser.get;
+browser.get = function (address) {
+  return hooked.apply(this, arguments).then(function (result) {
+    if (eyesOpen) {
+      eyes.checkWindow('get ' + address);
+    }
+    return result;
+  });
+};
+
+function eyesWith(fn) {
+  return function () {
+    var spec = fn.apply(this, arguments);
+    var hooked = spec.beforeAndAfterFns;
+    spec.beforeAndAfterFns = function () {
+      var result = hooked.apply(this, arguments);
+      result.befores.unshift({fn: function (done) {
+        eyesOpen = true;
+        eyes.open(browser, appName, spec.getFullName()).then(done);
+      }, timeout: () => 30000});
+      result.afters.unshift({fn: function (done) {
+        eyesOpen = false;
+        eyes.checkWindow('end');
+        eyes.close().then(done);
+      }, timeout: () => 30000});
+      return result;
+    };
+    return spec;
+  };
+}
+
+if (process.env.EYES_API_KEY) {
+  eyes.setApiKey(process.env.EYES_API_KEY);
+  eyes.it = eyesWith(it);
+  eyes.fit = eyesWith(fit);
+} else {
+  eyes.it = it;
+  eyes.fit = fit;
+}
+
+module.exports = eyes;
