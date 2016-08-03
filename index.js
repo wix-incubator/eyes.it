ac428d893061e50b27954d0d15495584a6f70a30
Ofir Attal <ofiratt@gmail.com>
2016-08-03 17:54:54 +0300

    Expose window size (#4)
    * Exposing window size argument as default value and per spec
    
    * update .gitignore
    
    * Not passing default screen size by default
    
    * Add optional: send null for default window size
    
    * Add default setting to eyes: batch name, save new test
    
    * Add default setting to eyes: batch name, save new test
    
    * Add default setting to eyes: batch name, save new test
    
    * Remove default save new test

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 /* global fit */
 'use strict';
 
-var Eyes = require('eyes.protractor').Eyes;
-var eyes = new Eyes();
 var path = require('path');
-
+var uuid = require('uuid');
+var Eyes = require('eyes.protractor').Eyes;
 var appName = require(path.join(process.cwd(), 'package.json')).name;
+var eyes = new Eyes();
+
 var eyesOpen = false;
 var hooked = browser.get;
 browser.get = function (address) {
@@ -22,15 +23,24 @@ function handleError(err, done) {
   done();
 }
 
+function isPassedWindowSizeArgument(argumentsObj) {
+  return typeof argumentsObj[2] === 'object'
+}
+
 function eyesWith(fn) {
   return function () {
+    var windowSize = eyes.defaultWindowSize;
+    if (isPassedWindowSizeArgument(arguments)) {
+      windowSize = arguments[2];
+      delete arguments[2];
+    }
     var spec = fn.apply(this, arguments);
     var hooked = spec.beforeAndAfterFns;
     spec.beforeAndAfterFns = function () {
       var result = hooked.apply(this, arguments);
       result.befores.unshift({fn: function (done) {
         eyesOpen = true;
-        eyes.open(browser, appName, 'eyes.it ' + spec.getFullName()).then(done);
+        eyes.open(browser, appName, 'eyes.it ' + spec.getFullName(), windowSize).then(done);
       }, timeout: () => 30000});
       result.afters.unshift({fn: function (done) {
         eyesOpen = false;
@@ -43,13 +53,24 @@ function eyesWith(fn) {
   };
 }
 
-if (process.env.EYES_API_KEY) {
-  eyes.setApiKey(process.env.EYES_API_KEY);
-  eyes.it = eyesWith(it);
-  eyes.fit = eyesWith(fit);
-} else {
-  eyes.it = it;
-  eyes.fit = fit;
+function _init() {
+  if (!process.env.EYES_BATCH_UUID) {
+    process.env.EYES_BATCH_UUID = uuid.v4();
+  }
+
+  if (process.env.EYES_API_KEY) {
+    eyes.setApiKey(process.env.EYES_API_KEY);
+    eyes.it = eyesWith(it);
+    eyes.fit = eyesWith(fit);
+  } else {
+    eyes.it = it;
+    eyes.fit = fit;
+  }
+
+  eyes.defaultWindowSize = null;
+  eyes.setBatch(appName, process.env.EYES_BATCH_UUID);
 }
+
+_init();
 
 module.exports = eyes;
