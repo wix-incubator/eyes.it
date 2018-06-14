1f392f92553fd83b8a8677c29c163f32f2f218c5
Erez Makavy <erezm@wix.com>
2018-06-14 10:14:48 +0300

    gitignore package-lock (+6 squashed commits) Squashed commits: [0c1cc52] Rename eyes-hook to eyes-augment [7e09ec6] nvmrc - increase to 8 [8e6178a] Update .nvmrc [b209595] Update eyes.e2e.js [f1813bb] Add yoshi. Add Tests. [beed974] Refactor - and add snapshot configuration

---
+++ src/eyes-augment.js
@@ -0,0 +1,67 @@
+const { eyes } = require('./eyes');
+const { eyesIt, init } = require('./eyes-it');
+
+function isPassedParameterArgument(argumentsObj) {
+  return typeof argumentsObj[2] === 'object';
+}
+
+function eyesWithout(fn) {
+  return function() {
+    if (isPassedParameterArgument(arguments)) {
+      delete arguments[2];
+    }
+    return fn.apply(this, arguments);
+  };
+}
+
+function extractArguments(args) {
+  const config = {};
+
+  if (isPassedParameterArgument(args)) {
+    const params = args[2];
+
+    // width or height of 0 will make the params window size to be ignored
+    if (params.width && params.height) {
+      config.windowSize = { width: params.width, height: params.height };
+    }
+
+    if (params.version) {
+      config.specVersion = params.version;
+    }
+
+    if (params.enableSnapshotAtEnd !== undefined) {
+      config.enableSnapshotAtEnd = params.enableSnapshotAtEnd;
+    }
+
+    if (params.enableSnapshotAtBrowserGet !== undefined) {
+      config.enableSnapshotAtBrowserGet = params.enableSnapshotAtBrowserGet;
+    }
+
+    delete args[2];
+  }
+
+  return config;
+}
+
+function eyesWith(fn) {
+  return function() {
+    const configFromArgs = extractArguments(arguments);
+    return eyesIt(fn, arguments, configFromArgs);
+  };
+}
+
+function augmentEyes() {
+  init();
+  eyes.it = eyesWith(it);
+  eyes.fit = eyesWith(fit);
+}
+
+/**
+ * Removes eyes.it 3rd argument to `it` funtion.
+ */
+function ignoreEyesIt() {
+  eyes.it = eyesWithout(it);
+  eyes.fit = eyesWithout(fit);
+}
+
+module.exports = { augmentEyes, ignoreEyesIt };
---
+++ test/eyes.e2e.js
@@ -0,0 +1,219 @@
+/**
+ * This is actually not an integration test, it is a unit-test.
+ * We only use the e2e suffix so that the default jasmine glob catchs this file.
+ */
+
+// Importing `../src/eyes` and not `../src`, since we don't want yet to initialize with `hookEyesIt()`.
+import { eyes } from '../src/eyes';
+import { augmentEyes } from '../src/eyes-augment';
+
+// This log array is filled by the mock functions, and asserted on after each test.
+let logs = [];
+function log(msg) {
+  logs.push(msg);
+}
+
+// Mocking `browser.get` needs to be done before calling `hookEyesIt()`.
+browser.get = () => {
+  log('browser_get');
+  return Promise.resolve();
+};
+
+// Now that `browser.get` is mocked, we can augment eyes
+augmentEyes();
+
+/** Mocks */
+eyes.open = (browser, appName, specName, windowSize) => {
+  log(`open(${specName})`);
+  return Promise.resolve();
+};
+
+eyes.checkWindow = msg => {
+  log(`checkWindow(${msg}}`);
+  return Promise.resolve();
+};
+
+eyes.close = () => {
+  log(`close`);
+  return Promise.resolve();
+};
+
+const STUB_URL = 'http://blah.com';
+
+/* Matchers */
+const OPEN = /^open/;
+const BROWSER_GET = /^browser_get/;
+const CHECK_WINDOW = /^checkWindow/;
+const DO_SOMETHING = /^do_something/;
+const CLOSE = /^close/;
+
+function doSomething() {
+  log('do_something');
+  return Promise.resolve();
+}
+
+function finalizeAndExpect(logMatchers) {
+  expect(logMatchers.length).toEqual(logs.length, 'boo'); // eslint-disable-line jest/prefer-to-have-length
+  logMatchers.forEach((matcher, index) => {
+    expect(logs[index]).toEqual(jasmine.stringMatching(matcher));
+  });
+  logs = [];
+}
+
+describe('eyes.it', () => {
+  describe('should create snapshot at end and browser.get', () => {
+    eyes.it('', async () => {
+      await browser.get(STUB_URL);
+      await doSomething();
+    });
+    afterAll(() => {
+      finalizeAndExpect([
+        OPEN,
+        BROWSER_GET,
+        CHECK_WINDOW,
+        DO_SOMETHING,
+        CHECK_WINDOW,
+        CLOSE,
+      ]);
+    });
+  });
+
+  describe('should not open eyes', () => {
+    it('', async () => {
+      await browser.get(STUB_URL);
+    });
+    afterAll(() => {
+      finalizeAndExpect([BROWSER_GET]);
+    });
+  });
+
+  describe('should NOT create any snapshots', () => {
+    eyes.it(
+      '',
+      async () => {
+        await doSomething();
+      },
+      {
+        enableSnapshotAtBrowserGet: false,
+        enableSnapshotAtEnd: false,
+      },
+    );
+    afterAll(() => {
+      finalizeAndExpect([OPEN, DO_SOMETHING, CLOSE]);
+    });
+  });
+
+  describe('should NOT create snapshot at end', () => {
+    eyes.it(
+      '',
+      async () => {
+        await doSomething();
+      },
+      {
+        enableSnapshotAtEnd: false,
+      },
+    );
+    afterAll(() => {
+      finalizeAndExpect([OPEN, DO_SOMETHING, CLOSE]);
+    });
+  });
+
+  describe('should create snapshot on browser.get inside beforeEach ', () => {
+    beforeEach(async () => {
+      await browser.get(STUB_URL);
+    });
+    eyes.it(
+      '',
+      async () => {
+        await doSomething();
+      },
+      {
+        enableSnapshotAtEnd: false,
+      },
+    );
+    afterAll(() => {
+      finalizeAndExpect([OPEN, BROWSER_GET, CHECK_WINDOW, DO_SOMETHING, CLOSE]);
+    });
+  });
+
+  describe('should create snapshot on browser.get inside it ', () => {
+    eyes.it(
+      '',
+      async () => {
+        await doSomething();
+        await browser.get(STUB_URL);
+      },
+      {
+        enableSnapshotAtEnd: false,
+      },
+    );
+    afterAll(() => {
+      finalizeAndExpect([OPEN, DO_SOMETHING, BROWSER_GET, CHECK_WINDOW, CLOSE]);
+    });
+  });
+
+  describe('should create snapshot in afterEach block', () => {
+    eyes.it(
+      '',
+      async () => {
+        await browser.get(STUB_URL);
+        await doSomething();
+      },
+      {
+        enableSnapshotAtBrowserGet: false,
+        enableSnapshotAtEnd: false,
+      },
+    );
+
+    afterEach(() => {
+      eyes.checkWindow('blah');
+    });
+
+    afterAll(() => {
+      finalizeAndExpect([OPEN, BROWSER_GET, DO_SOMETHING, CHECK_WINDOW, CLOSE]);
+    });
+  });
+
+  describe('should create snapshot at End, before afterEach block', () => {
+    eyes.it(
+      '',
+      async () => {
+        await browser.get(STUB_URL);
+        await doSomething();
+      },
+      {
+        enableSnapshotAtBrowserGet: false,
+      },
+    );
+
+    afterEach(async () => {
+      await doSomething();
+    });
+
+    afterAll(() => {
+      finalizeAndExpect([
+        OPEN,
+        BROWSER_GET,
+        DO_SOMETHING,
+        CHECK_WINDOW,
+        DO_SOMETHING,
+        CLOSE,
+      ]);
+    });
+  });
+
+  describe('should open eyes with specName and version', () => {
+    eyes.it('my spec', async () => {}, {
+      enableSnapshotAtBrowserGet: false,
+      enableSnapshotAtEnd: false,
+      version: '1.0.0',
+    });
+
+    afterAll(() => {
+      finalizeAndExpect([
+        /^open\(eyes\.it eyes\.it should open eyes with specName and version my spec version: 1\.0\.0\)/,
+        CLOSE,
+      ]);
+    });
+  });
+});
