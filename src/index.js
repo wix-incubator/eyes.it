988dac0c108064245730cf2f174eb27161942a5a
Erez Makavy <erezm@wix.com>
2018-11-28 21:16:34 +0200

    Add eyesItInstance

--- src/index.js
+++ src/index.js
@@ -9,4 +9,21 @@ if (shouldAugment) {
   ignoreEyesIt();
 }
 
-module.exports = eyes;
+/**
+ * Create an instance of eyes.it with a default configure options.
+ * All instances share the same eyes singleton which is already initialized.
+ */
+export function eyesItInstance(config) {
+  /* eslint-disable no-restricted-globals */
+  return new Proxy(eyes, {
+    /* eslint-enable no-restricted-globals */
+    get: (obj, prop) => {
+      if (prop === 'it' || prop === 'fit') {
+        return (msg, f) => obj[prop](msg, f, config);
+      }
+      return obj[prop];
+    },
+  });
+}
+
+export default eyes;
