526d13cf5b2160dd67fce8eb581a886383f4c8f1
Jonathan Adler <jonathana@wix.com>
2020-02-24 12:25:45 +0200

    add real e2e test

--- test/index.e2e.js
+++ test/index.e2e.js
@@ -1,5 +1,21 @@
 import eyes from '../src';
+import { MockServer } from './mockServer';
 
 describe('eyes.it', function() {
-  eyes.it('should not throw error', () => {});
+  let server = new MockServer();
+
+  beforeEach(() => {
+    server.start();
+  });
+
+  afterEach(() => {
+    server.stop();
+  });
+
+  eyes.it('should not throw error', async () => {
+    // // fixes protractor angular related exception: See http://git.io/v4gXM for details.
+    // browser.ignoreSynchronization = true;
+    browser.waitForAngularEnabled(false);
+    await browser.get(`${MockServer.URI}/`);
+  });
 });
