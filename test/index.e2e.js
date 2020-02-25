24d2ad8006d0ca73b4cd892b5d52d41d0c83664b
Jonathan Adler <jonathana@wix.com>
2020-02-25 17:16:20 +0200

    remove comments

--- test/index.e2e.js
+++ test/index.e2e.js
@@ -13,8 +13,6 @@ describe('eyes.it', function() {
   });
 
   eyes.it('should not throw error', async () => {
-    // // fixes protractor angular related exception: See http://git.io/v4gXM for details.
-    // browser.ignoreSynchronization = true;
     browser.waitForAngularEnabled(false);
     await browser.get(`${MockServer.URI}/`);
   });
